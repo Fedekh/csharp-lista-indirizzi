e879cb3b71478673b0df3a269eb4fc456c476557
Federico Ceteroni <developer@overside.it>
2023-09-19 15:28:17 +0200

    conclude

---
+++ csharp-lista-indirizzi/Clienti.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Clienti
+    {
+        private string Name { get; set; }
+        private string Lastname { get; set; }
+        private string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Zip { get; set; }
+
+        public Clienti(string name, string lastname, string address, string city, string province, string zip)
+        {
+            this.Name = name;
+            this.Lastname = lastname;
+            this.Address = address;
+            this.City = city;
+            this.Province = province;
+            this.Zip = zip;
+        }
+
+        public override string ToString() //sovrascriviamo il metodo di c#
+        {
+            return $"\n\t-Nome:i {this.Name}\n\t-Cognome: {this.Lastname}\n\t-Via: {this.Address}\n\t-Citta: {this.City}\n\t-Provincia: {this.Province}\n\t-CAP: {this.Zip}";
+        }
+    }
+}
+
+
+
+
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,12 +1,94 @@
-﻿using System;
-
+﻿using csharp_lista_indirizzi;
+using System;
+using System.IO;
+ 
 namespace csharplistaindirizzi
 {
     class Program
     {
         public static void Main(string[] args)
         {
+            string path = @"C:\Users\Federico\Documents\Development\csharp-lista-indirizzi\csharp-lista-indirizzi\data\my-addresses.csv";
 
+            /* LEGGERE UN FILE
+             *  
+             *  1) string file = File.ReadAllText(path);
+             *   Console.WriteLine(file);
+             * 
+             *  2) StreamReader file = File.OpenText(path);
+             *   while (!file.EndOfStream)
+             *   {
+             *   string riga = file.ReadLine();
+             *      Console.WriteLine(riga);
+             *   }
+             *   file.Close();
+             *
+             *
+             *  SCRIVERE SU UN FILE
+             *  
+             *  if (!File.Exists(path))
+             *  {
+                    StreamWriter file = File.CreateText(path);
+                    file.WriteLine("Hello");
+                    file.WriteLine("And");
+                    chiudo il mio file
+                    file.Close();
+                }
+             **/
+            List<Clienti> myClient = new List<Clienti>();
+
+            try
+            {
+                StreamReader file = File.OpenText(path);
+                int i = 0;
+                Console.WriteLine("Ecco la lista dei clienti nel file originale :");
+                Console.WriteLine();
+                while (!file.EndOfStream)
+                {
+                    string righe = file.ReadLine();                  // ogni singola riga, con ciclo, si stampano tutte le righe del file
+                    i++;
+                    if (i > 1)                                          // Non prendo l'header della tabella
+                    {
+                        string[] splitRiga = righe.Split(',');
+
+                        if (splitRiga.Length < 6)
+                        {
+                            Console.WriteLine($"{i-1} C'è qualche campo mancante");
+                        }
+                        else
+                        {
+                        string name = splitRiga[0].TrimStart(new char[]{' ', ',' });
+                        string lastname= splitRiga[1];
+                        string address = splitRiga[2];
+                        string city = splitRiga[3];
+                        string province = splitRiga[4];
+                        string zip = splitRiga[5];
+
+                        Clienti newCliente = new Clienti(name, lastname, address, city, province, zip);
+                        myClient.Add(newCliente);
+
+                        Console.WriteLine($"{i-1}: {righe}");
+
+                             
+                        }
+                        //Console.WriteLine(name);
+                    }
+                }
+                file.Close();
+
+
+
+                Console.WriteLine(myClient.Count);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            for (int i = 0; i < myClient.Count; i++)
+            {
+                Console.WriteLine($"{i+1} {myClient[i]}");
+            }
         }
     }
 }
